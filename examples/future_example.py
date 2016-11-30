bafe7f91427c5f33643d22a12e62b8c74620e009
Kenneth Lyons <ixjlyons@gmail.com>
2016-11-30 09:31:00 -0800

    More work on future example.

--- examples/future_example.py
+++ examples/future_example.py
@@ -3,11 +3,16 @@ This is a pseudo-code-ish example for me to figure out how making a custom
 experiment should look once the library is more complete.
 """
 
+gesture_mapping = {
+    0: 'rest',
+    1: 'open-hand',
+    2: 'close-hand'
+}
 
-class TrainTask(ExperimentTask):
 
-    def __init__(self, task_storage):
-        self.task_storage = task_storage
+# 1. define custom tasks
+#   - each custom task should be in a separate module or maybe all custom
+#     custom tasks could be put in a single module `tasks.py`
 
 
 class TACTask(ExperimentTask):
@@ -21,6 +26,14 @@ class TACTask(ExperimentTask):
         data = self.train_storage.collect_sessions(self._subject,
                                                    self._selected_sessions)
         self.pipeline.named_blocks('classifier').fit(*data)
+
+
+# 2. write functions to set up each task
+#   - task implementations should be fairly general (e.g. a specific pipeline
+#     isn't enforced), so some things need to be set up to be passed in to
+#     the tasks __init__
+#   - having a separate function to do the setup allows `main()` to look like
+#     a high level overview of the task
 
 
 def setup_oscilloscope(daq):
@@ -40,8 +53,14 @@ def setup_oscilloscope(daq):
 
 
 def setup_train_task(daq, task_storage):
+    def img_path(name):
+        return pkg_resources.resource_filename(
+            os.path.join(__name__, 'images', name+'.png'))
+
     pipeline = Pipeline([daq])
-    return TrainTask(pipeline, task_storage)
+    imgs = [l: img_path(n) for l, n in gesture_mapping.items()]
+
+    return ImageTrainingTask(pipeline, images=imgs, task_storage=task_storage)
 
 
 def setup_tac_task(daq, task_storage, train_storage):
@@ -61,7 +80,13 @@ def setup_tac_task(daq, task_storage, train_storage):
     return TACTask(pipeline, task_storage, train_storage)
 
 
-if __name__ == '__main__':
+# 3. write an entry point
+#   - set up storage and a data source
+#   - initialize the main application
+#   - set up and install the tasks
+
+
+def main():
     daq = EmulatedDaq(rate=1000, num_channels=2, read_size=100)
 
     db = ExperimentDatabase('file.hdf5', 'a')
@@ -72,3 +97,7 @@ if __name__ == '__main__':
         app.install_task(setup_oscilloscope(daq))
         app.install_task(setup_train_task(daq, train_group))
         app.install_task(setup_tac_task(daq, tac_group, train_group))
+
+
+if __name__ == '__main__':
+    main()
