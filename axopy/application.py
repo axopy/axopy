b15a99567bf080d17b5d0ea0c5be64442a07ce1b
Kenneth Lyons <ixjlyons@gmail.com>
2017-02-01 18:17:01 -0800

    Initial work on Qt-backed experiment.

--- axopy/application.py
+++ axopy/application.py
@@ -1,192 +1,66 @@
+import sys
 from contextlib import contextmanager
 from PyQt5 import QtCore, QtWidgets, QtGui
-import copper
-
-from axopy import daq
 
 
-@contextmanager
-def application(*args, **kwargs):
-    """Convenience context manager for running an AxoPy application.
+qtapp = None
 
-    The application instance is created on entry and executed on exit. The
-    arguments and keyword arguments are passed along to :class:`BaseUI`, and
-    the :class:`BaseUI` instance is yielded.
 
-    Examples
-    --------
-    This is a sort of pseudo-code example showing how to use the application
-    context manager. A couple custom tasks are installed, and the application
-    runs upon exit from the context.
+def get_qtapp():
+    """Gets a `QApplication` instance running.
 
-    >>> from axopy.application import application
-    >>> with application(daq, db) as app:
-    ...     app.install_task(CustomTask())
-    ...     app.install_task(AnotherCustomTask())
+    Returns the current QApplication instance if it exists and creates it
+    otherwise.
     """
-    app = QtWidgets.QApplication([])
-    ui = BaseUI(*args, **kwargs)
-    yield ui
-    ui.show()
-    app.exec_()
+    global qtapp
+    inst = QtWidgets.QApplication.instance()
+    if inst is None:
+        qtapp = QtWidgets.QApplication(sys.argv)
+    else:
+        qtapp = inst
+    return qtapp
 
 
-class Task(QtWidgets.QWidget):
-    """Base implementation of a task.
+class QtExperimentBackend(QtWidgets.QMainWindow):
+    """The Qt-backed implementation of an experiment.
 
-    A task is the core unit of operation in an experiment workflow. Each task
-    is presented as a tab in the :class:`BaseUI`.
+    Provides a QMainWindow with a tab widget, where each task gets its own tab
+    in the interface.
     """
 
-    requires_participant = False
+    def __init__(self):
+        super(QtExperimentBackend, self).__init__()
 
-    def setup_ui(self, parent):
-        """Initializes the ``QWidget`` for the task and returns it.
-
-        Parameters
-        ----------
-        parent : QWidget
-            Parent widget of the task.
-
-        Returns
-        -------
-        widget : QWidget
-            The task user interface widget.
-        """
-        pass
-
-    def setup_daq(self, daq):
-        """Initialize the data acquisition thread.
-
-        This method is automatically called when the task is shown. Subclasses
-        requiring use of a data acquisition device should override this method
-        and perform the following:
-
-            1. set up the thread's processing pipeline
-            2. connect the thread's callbacks to methods for handling data
-            3. start the thread
-        """
-        pass
-
-    def setup_storage(self, task_storage, dependencies=None):
-        """Initialize data storage for the task.
-
-        This method is automatically called when the task is shown. Subclasses
-        requiring use of data storage should override this method.
-
-        Parameters
-        ----------
-        task_storage : TaskStorage
-            Storage belonging to the task.
-        dependencies : dict, optional
-            Dictionary of :class:`Storage` objects the task depends on,
-            accessed by installed name.
-        """
-        pass
-
-    def shutdown_daq(self):
-        """Shut down the data acquisition thread.
-
-        This method is automatically called when the task is hidden.
-        Subclasses requiring use of a data acquisition device should override
-        this method and perform the following:
-
-            1. disconnect callbacks connected in `setup_daq`
-            2. kill the thread
-        """
-        pass
-
-    def showEvent(self, event):
-        """Callback for when the task becomes visible.
-
-        Tasks that become visible check if their requirements are met, and if
-        not, disable themselves.
-        """
-        if self.requires_participant:
-            if self.base_ui.participant is None:
-                self._participant_requirement_warning()
-                self.setEnabled(False)
-                return
-
-        self.setEnabled(True)
-        self.setup_daq()
-        self.setup_storage()
-
-    def hideEvent(self, event):
-        self.shutdown_daq()
-
-    def _participant_requirement_warning(self):
-        """Shows a warning QMessageBox when a participant is required."""
-        QtWidgets.QMessageBox().warning(
-            self,
-            "Warning",
-            "{} requires a participant to be selected.".format(str(self)),
-            QtWidgets.QMessageBox.Ok)
-
-    def _daq_requirement_warning(self):
-        """Shows a warning QMessageBox when a DAQ is required."""
-        QtWidgets.QMessageBox().warning(
-            self,
-            "Warning",
-            "{} requires a data acquisition device to be present.".format(
-                str(self)),
-            QtWidgets.QMessageBox.Ok)
-
-    def __str__(self):
-        return self.__class__.__name__
-
-
-class BaseUI(QtWidgets.QMainWindow):
-    """The base user interface for running experiments.
-
-    Parameters
-    ----------
-    daq : Daq
-        Data acquisition device.
-    participant_dialog : class, optional
-        A class derived from NewParticipantDialog. By default, a simple dialog
-        is shown which contains a single entry for the participant ID.
-
-    Attributes
-    ----------
-    daq_thread : DaqThread
-        Thread running the data acquisition device.
-    participant : str
-        Currently selected participant.
-    """
-
-    def __init__(self, daq, database, parent=None):
-        super(BaseUI, self).__init__(parent)
         self._setup_ui()
-
-        self.daq = daq
-        self.database = database
-
-        # populate participant list from database
-        #for p in self.database.get_participants():
-        #    self._particpiant_selector.add_participant(p)
-
         self.tasks = {}
-        self.participant = None
-        #self.daq_thread = DaqThread(daq)
 
-    def install_task(self, task, name=None):
+        self.show()
+
+    def add_task(self, task_ui, name):
         """Add a task to the UI.
 
         Parameters
         ----------
-        task: Task
-            Any task extending the base TaskUI class.
-        name : str, optional
-            Name of the task. Used as the tab label and in the data storage
-            hierarchy. By default, the Task's ``str`` is used.
+        task_ui: QWidget
+            Any widget representing a tasks interface.
+        name : str
+            Name of the task. Used as the tab label.
         """
-        if name is None:
-            name = str(task)
+        self.tasks[name] = task_ui
+        self._tab_widget.addTab(task_ui, name)
 
-        widget = task.setup_ui()
-        self.tasks[name] = task
-        self._tab_widget.addTab(widget, name)
+    def set_status(self, message):
+        """Adds a status message to the status bar.
+
+        This is typically used for showing the current subject and session
+        information.
+
+        Parameters
+        ----------
+        message : str
+            Message to display in the status bar.
+        """
+        self._statusbar_label.setText(message)
 
     def _setup_ui(self):
         """Initialize widgets and callbacks."""
@@ -197,14 +71,7 @@ class BaseUI(QtWidgets.QMainWindow):
 
         # tab widget -- each tab holds a task
         self._tab_widget = QtWidgets.QTabWidget(central_widget)
-        self._tab_widget.setMovable(True)
         main_layout.addWidget(self._tab_widget)
-
-        # participant selection task
-        self._particpiant_selector = ParticipantSelector()
-        self._particpiant_selector.selected.connect(
-            self._on_participant_selected)
-        self._tab_widget.addTab(self._particpiant_selector, "Participant")
 
         # button box for controlling tasks
         self._buttons = _ButtonControls()
@@ -216,15 +83,8 @@ class BaseUI(QtWidgets.QMainWindow):
         # status bar shows current participant
         status_bar = QtWidgets.QStatusBar(self)
         self.setStatusBar(status_bar)
-        self._statusbar_label = QtWidgets.QLabel("no participant selected")
+        self._statusbar_label = QtWidgets.QLabel("status")
         status_bar.addPermanentWidget(self._statusbar_label)
-
-    def _on_participant_selected(self, pid):
-        """Callback called when a participant is selected."""
-        self._statusbar_label.setText("participant: {}".format(pid))
-        self.participant = pid
-
-        # enable tabs in the tabwidget for tasks requiring a participant
 
     def _on_start_clicked(self):
         print("start")
@@ -294,6 +154,12 @@ class ParticipantSelector(QtWidgets.QWidget):
         self.new_button = QtWidgets.QPushButton("New Participant")
         self.new_button.clicked.connect(self._on_new_participant)
         self.main_layout.addWidget(self.new_button, 2, 0, 1, 1)
+
+        self._buttons = _ButtonControls()
+        self._buttons.start_clicked.connect(self._on_start_clicked)
+        self._buttons.stop_clicked.connect(self._on_stop_clicked)
+        main_layout.addWidget(self._buttons)
+        self._buttons.setEnabled(False)
 
     def _on_new_participant(self):
         dialog = NewParticipantDialog(extra_attrs=[('hand', 'Handedness')])
@@ -402,83 +268,7 @@ class NewParticipantDialog(QtWidgets.QDialog):
         return {a: str(e.text()) for a, e in self.line_edits.items()}
 
 
-class DaqThread(QtCore.QThread):
-    """A QThread for polling data from a data acquisition device.
-
-    Attributes
-    ----------
-    updated : pyqtSignal
-        Emits the latest data from the data acquisition unit as processed by
-        the pipeline.
-    disconnected : pyqtSignal
-        Emitted when there is a problem with the data acquisition unit.
-    """
-
-    updated = QtCore.pyqtSignal(object)
-    disconnected = QtCore.pyqtSignal()
-
-    def __init__(self, daq, pipeline=None, parent=None):
-        super(DaqThread, self).__init__(parent=parent)
-        self.daq = daq
-
-        self._running = False
-
-        if pipeline is None:
-            self.pipeline = self._default_pipeline()
-        else:
-            self.pipeline = pipeline
-
-    @property
-    def pipeline(self):
-        return self._pipeline
-
-    @pipeline.setter
-    def pipeline(self, pipeline):
-        was_running = False
-        if self._running:
-            was_running = True
-            self.kill()
-
-        self._pipeline = pipeline
-
-        if was_running:
-            self.start()
-
-    def remove_pipeline(self):
-        self.pipeline = self._default_pipeline()
-
-    @property
-    def running(self):
-        return self._running
-
-    def run(self):
-        self._running = True
-
-        if self._pipeline is not None:
-            self._pipeline.clear()
-
-        self.daq.start()
-
-        while True:
-            if not self._running:
-                break
-
-            try:
-                d = self.daq.read()
-            except daq.DisconnectException:
-                self.disconnected.emit()
-                return
-
-            if self._pipeline is not None:
-                self.updated.emit(self._pipeline.process(d))
-            else:
-                self.updated.emit(d)
-
-        self.daq.stop()
-
-    def kill(self):
-        self._running = False
-        self.wait()
-
-    def _default_pipeline(self):
-        return copper.Pipeline(copper.PipelineBlock())
+if __name__ == '__main__':
+    app = get_qtapp()
+    exp = QtExperimentBackend()
+    app.exec_()
