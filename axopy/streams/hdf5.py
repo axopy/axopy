c99e77aefbbc86fbe6f1af3b3bb5e831b0379daa
Kenneth Lyons <ixjlyons@gmail.com>
2017-02-08 01:03:19 -0800

    Started hdf5 storage backend.

--- axopy/streams/hdf5.py
+++ axopy/streams/hdf5.py
@@ -1,188 +1,150 @@
-"""
-Basic data storage framework backed by h5py.
-"""
-
 import h5py
 import datetime
 
 
-class ExperimentDatabase(h5py.File):
-    """Top-level storage unit for an experiment backed by HDF5.
+class HDF5Node:
+    """Individual destination/source that a task can stream to/from.
+
+    An example would be a task where you want to store an array of raw input
+    data along with some event data for each trial. Each of those distinct
+    "arrays" would get their own stream.
     """
-
-    def create_participant(self, pid):
-        """Create a group in the file to hold all of a participant's data.
-
-        Parameters
-        ----------
-        pid : str
-            Participant identifier. Used as the group name.
-        """
-        return self.create_group(participant_path(pid))
-
-    def get_participant(self, pid):
-        """Retrive the group in the file holding all of a participant's data.
-
-        Parameters
-        ----------
-        pid : str
-            Participant identifier.
-        """
-        return self.get(participant_path(pid))
-
-    def require_participant(self, pid):
-        """Retrieve the group in the file holding all of the participant's data.
-
-        If the participant is not already registered in the file, a group is
-        created.
-
-        Parameters
-        ----------
-        pid : str
-            Participant identifier.
-        """
-        return self.require_group(participant_path(pid))
-
-    def create_task(self, pid, name):
-        """Instantiate a task for a participant.
-
-        If the task (by name) has been installed into the experiment storage
-        file, an object of that task storage class is returned. Otherwise, a
-        :class:`BaseTaskStorage` object is returned.
-
-        Parameters
-        ----------
-        pid : str
-            Participant ID to creae the task for.
-        name : str
-            Name of the task.
-        """
-        return TaskStorage(self.create_group(task_path(pid, name)))
-
-    def get_task(self, pid, name):
-        """Retrieves a task storage group for a participant.
-
-        If the task (by name) has been installed into the experiment storage
-        file, an object of that task storage class is returned. Otherwise, a
-        :class:`BaseTaskStorage` object is returned.
-
-        Parameters
-        ----------
-        pid : str
-            Participant ID to creae the task for.
-        name : str
-            Name of the task.
-        """
-        return TaskStorage(self.get(task_path(pid, name)))
-
-    def require_task(self, pid, name):
-        """Retrieves a task storage group for a participant.
-
-        If the task group is not already registered in the file, a group is
-        created.
-
-        If the task (by name) has been installed into the experiment storage
-        file, an object of that task storage class is returned. Otherwise, a
-        :class:`BaseTaskStorage` object is returned.
-
-        Parameters
-        ----------
-        pid : str
-            Participant ID to creae the task for.
-        name : str
-            Name of the task.
-        """
-        return TaskStorage(self.require_group(task_path(pid, name)))
-
-    def get_participants(self):
-        return list(self.keys())
+    pass
 
 
+# TODO: add **attrs to each of the `require_<level>` methods
+# TODO: make `current_<level>` attributes iterable somehow
+# TODO: consider making `current_<level>` attrs properties with side effects
 
-class TaskStorage(object):
-    """A simple storage scheme suitable for many tasks.
+class HDF5Storage(h5py.File):
+    """Top-level storage unit for an experiment backed by HDF5.
 
-    Each session gets its own group, and each trial of the session consists of
-    a single dataset (a single homogeneous array). This storage scheme is
-    suitable for tasks involving raw data capture or tasks meant for simple
-    processing of raw data (e.g. signal conditioning) or feature computation.
+    An `HDF5Storage` object can be used for both creating an experiment dataset
+    and reading an existing one.
 
-    This class can be used both for saving new data (create mode) or loading
-    existing data (get mode). To use create mode, pass a Group object that
-    create yourself. To use get mode, pass a Group object that already exists
-    in storage.
-
-    Parameters
-    ----------
-    group : h5py.Group
-        The group belonging to the task.
+    `HDF5Storage` maintains a set of attributes for keeping track of the
+    current location in the dataset hierarchy for convenience. These should
+    always be in agreement such that you could combine them together to form
+    the path to the current trial.
 
     Attributes
     ----------
-    current_session: h5py.Group
-        The subgroup of the task group representing the current session for the
-        task. In create mode, the most recently created session is used. In get
-        mode, the most recently retrieved session is used.
+    current_participant : str
+        ID of the currently selected participant.
+    current_task : str
+        Name of the currently selected task.
+    current_run : str
+        Name of the currently selected run.
+    current_trial : str
+        Name of the currently selected trial.
     """
 
-    def __init__(self, group, trial_cls=None):
-        self.group = group
-        self.current_session = None
+    def __init__(self, *args, **kwargs):
+        super(HDF5Storage, self).__init__(*args, **kwargs)
 
-    def get_session(self, name):
-        session = self.group.get(name)
-        self.current_session = session
-        return self.current_session
+        self.current_participant = None
+        self.current_task = None
+        self.current_run = None
+        self.current_trial = None
 
-    def create_session(self, name):
-        session = self.group.create_group(name)
-        session.attrs['date'] = dt()[0]
-        self.current_session = session
-        return self.current_session
+    def require_participant(self, participant):
+        """Retrieve the group in the file holding all of the participant's data.
 
-    def _get_session(self, session=None):
-        if session is None:
-            session = self.current_session
+        If the participant is not already registered in the file, a group is
+        created (assuming file mode includes writing). This also sets the
+        currently selected participant.
 
-        if isinstance(session, str):
-            session = self.get_session(session)
+        Parameters
+        ----------
+        participant : str
+            Participant identifier.
+        """
+        self.current_participant = participant
+        return self.require_group(participant)
 
-        return session
+    def require_task(self, task, participant=None):
+        """Retrieves a task storage group for a participant.
 
-    def create_trial(self, name, session=None):
-        session = self._get_session(session)
-        trial = session.create_group(name)
-        return TrialStorage(trial)
+        If the task group is not already registered in the file, a group is
+        created (assuming file mode includes writing). This also sets the
+        currently selected task as well as the current participant (if
+        provided).
 
-    def get_trial(self, name, session=None):
-        session = self._get_session(session)
-        trial = session.get(name)
-        return trial
+        Parameters
+        ----------
+        task : str
+            Name of the task.
+        participant : str, optional
+            Participant ID. If not provided, the currently selected participant
+            ID is used.
+        """
+        participant = self._check_current('participant', participant)
+        self.current_task = task
+        grp = self.require_participant(participant)
+        return grp.require_group(task)
 
+    def require_run(self, run, task=None, participant=None):
+        """Retrieves a run storage group for a task.
 
-class TrialStorage(dict):
+        If the run group is not already registered in the file, a group is
+        created (assuming file mode includes writing). This also sets the
+        currently selected run, task, and participant.
 
-    def __init__(self, group):
-        self.group = group
+        Parameters
+        ----------
+        run : str
+            Name of the run to create.
+        task : str, optional
+            Name of the task. If not provided, the currently selected task is
+            used.
+        participant : str, optional
+            Participant ID. If not provided, the currently selected participant
+            ID is used.
+        """
+        task = self._check_current('task', task)
+        self.current_run = run
+        grp = self.require_task(task, participant=participant)
+        return grp.require_group(run)
 
-    def __getitem__(self, key):
-        return self.group.attrs[key]
+    def require_trial(self, trial, run=None, task=None, participant=None):
+        """Retrieves a trial storage group for a run.
 
-    def __setitem__(self, key, val):
-        self.group.attrs[key] = val
+        If the trial group is not already registered in the file, a group is
+        created (assuming file mode includes writing). This also sets the
+        currently selected trial, run, task, and participant.
 
-    def create_dataset(self, name, data, attrs=None):
-        ds = self.group.create_dataset(name, data=data)
-        for key, val in attrs.items():
-            ds.attrs[key] = val
+        Parameters
+        ----------
+        trial : str
+            Name of the trial to create.
+        run : str
+            Name of the run to create.
+        task : str, optional
+            Name of the task. If not provided, the currently selected task is
+            used.
+        participant : str, optional
+            Participant ID. If not provided, the currently selected participant
+            ID is used.
+        """
+        run = self._check_current('run', run)
+        self.current_trial = trial
+        grp = self.require_run(run, task=task, participant=participant)
+        return grp.require_group(trial)
 
+    def _check_current(self, level, val):
+        """Ensures either the current value of a level in the hierarchy is set
+        or provided.
+        """
+        self_val = getattr(self, 'current_{}'.format(level))
+        if val is None:
+            if self_val is None:
+                raise ValueError("Must provide a {} since none has been"
+                                 " selected".format(level))
+            val = self_val
+        return val
 
-def participant_path(pid):
-    return '{}'.format(pid)
-
-
-def task_path(pid, experiment_id):
-    return '{}/{}'.format(participant_path(pid), experiment_id)
+    def get_participants(self):
+        return list(self.keys())
 
 
 def dt():
