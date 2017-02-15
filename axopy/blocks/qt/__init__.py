00950cf4ed2b9e6a6fa72534be0561c6591dad40
Kenneth Lyons <ixjlyons@gmail.com>
2017-02-14 16:00:14 -0800

    Merged in work on qt experiment and messaging.

---
+++ axopy/blocks/qt/__init__.py
@@ -0,0 +1,15 @@
+from axopy.qt.base import WidgetBlock
+from axopy.qt.prompts import (ImagePrompt, AnnotatedProgressBar,
+                              ImageDeck, LabelDeck)
+from axopy.qt.signals import OscilloscopeWidget
+from axopy.qt.cursor import (CursorInterface, CircleItem,
+                             PositionAnimation)
+
+__all__ = ['ImagePrompt',
+           'AnnotatedProgressBar',
+           'ImageDeck',
+           'LabelDeck',
+           'OscilloscopeWidget',
+           'CursorInterface',
+           'CircleItem',
+           'PositionAnimation']
