1bce1188cd6b845f7e06356c3d083ada3b9d046d
ben <ben@sixpearls.com>
2017-05-08 12:20:15 -0700

    more involved test

--- tests/test_messaging_qt.py
+++ tests/test_messaging_qt.py
@@ -1,26 +1,46 @@
 from axopy.messaging import emitter, receiver
 
-class FakeEmitterBlock(object):
-    @emitter(data=str)
-    def fake_emitter(self, data):
-        print("emitting: {}".format(data))
+class FakeBlock1(object):
+    @emitter(data=str) # I don't think this is enforced anywhere
+    def emitter1(self, data):
+        print("emitting1: {}".format(data))
         return data
 
-class FakeReceiverBlock(object):
     @receiver
-    def fake_receiver(self, data):
-        print("recieved: {}".format(data))
+    def receiver2(self, data):
+        print("recieved2: {}".format(data))
+        
+
+class FakeBlock2(object):
+    def __init__(self):
+        self.number_called = 0
+
+    @receiver
+    def receiver1(self, data):
+        print("recieved1: {}".format(data))
+        self.number_called += 1
+        self.emitter2(self.number_called)
+        
+
+    @emitter(number=int) # I don't think this is enforced anywhere
+    def emitter2(self, number):
+        print("emitting2: {}".format(number))
+        return number
+
 
 if __name__ == '__main__':
     print("in main, init blocks")
-    block1 = FakeEmitterBlock()
-    block2 = FakeReceiverBlock()
+    block1 = FakeBlock1()
+    block2 = FakeBlock2()
 
     print("connecting")
     # block1.fake_emitter.connect(block2.fake_receiver)
-    block2.fake_receiver.connect(block1.fake_emitter)
-    print(block1.fake_emitter.data_format)
+    block2.receiver1.connect(block1.emitter1)
+    block2.emitter2.connect(block1.receiver2)
+
+    print("block1:",block1.emitter1.data_format)
+    print("block2:",block2.emitter2.data_format)
     
 
     print("calling emitter from main")
-    block1.fake_emitter("sending some stuff")
+    block1.emitter1("sending some stuff")
