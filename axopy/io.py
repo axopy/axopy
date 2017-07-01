0400d6ff3e9374b2045674ae90162ffe3c4899c8
Kenneth Lyons <ixjlyons@gmail.com>
2017-07-01 16:20:17 -0700

    A little cleanup.

--- axopy/io.py
+++ axopy/io.py
@@ -1,8 +1,20 @@
 import time
-import numpy as np
-from expyriment.io._input_output import Input
+import numpy
 
-class MouseInterface(object):
+
+class Mouse(object):
+    """Mouse input provided by expyriment with a blocking interface.
+
+    Parameters
+    ----------
+    mouse : expyriment.io.Mouse
+        The mouse device provided by expyriment. This object can be obtained
+        from an ``expyriment.design.Experiment`` object.
+    rate : float, optional
+        Rate (in Hz) of input update. To simulate a blocking data acquisition
+        device, the device sleeps in between updates so as to provide the
+        specified update rate.
+    """
 
     def __init__(self, mouse, rate=10):
         self.mouse = mouse
@@ -13,12 +25,23 @@ class MouseInterface(object):
         self._t_per_read = float(1 / self.rate)
 
     def start(self):
+        """Starts the device recording. Not necessary for this device."""
         pass
 
     def stop(self):
+        """Stops the device recording. Not necessary for this device."""
         pass
 
     def read(self):
+        """Grab the current mouse position on screen.
+
+        Position is referenced to the center of the expyriment window/screen.
+
+        Returns
+        -------
+        position : ndarray, shape (2,)
+            2-element array with the x and y position of the mouse.
+        """
         t = time.time()
         if self._t_last_read is None:
             time.sleep(self._t_per_read)
@@ -29,24 +52,27 @@ class MouseInterface(object):
                 # if we're not meeting real-time requirement, don't wait
                 pass
 
-        data = np.asarray(self.mouse.position)
+        data = numpy.asarray(self.mouse.position)
 
         self._t_last_read = time.time()
         return data
 
+    def reset(self):
+        """Resets the device parameters. Not necessary for this device."""
+        pass
 
-class EmulatedDaq(Input):
-    """
-    An emulated data acquisition device which generates random data.
+
+class EmulatedDaq(object):
+    """An emulated data acquisition device which generates random data.
 
     Each sample of the generated data is sampled from a zero-mean Gaussian
     distribution with variance determined by the amplitude specified, which
     corresponds to three standard deviations. That is, approximately 99.7% of
     the samples should be within the desired peak amplitude.
 
-    :class:`EmulatedDaq` is meant to emulate data acquisition devices that
-    block on each request for data until the data is available. See
-    :meth:`read` for details.
+    ``EmulatedDaq`` is meant to emulate data acquisition devices that block on
+    each request for data until the data is available. See
+    ``readx` for details.
 
     Parameters
     ----------
@@ -59,13 +85,11 @@ class EmulatedDaq(Input):
         amplitude represents three standard deviations for generating the
         Gaussian distributed data. Default is 1.
     read_size : int, optional
-        Number of samples to generate per :meth:`read()` call. Default is 100.
+        Number of samples to generate per ``read()`` call. Default is 100.
     """
 
     def __init__(self, rate=1000, num_channels=1, amplitude=1.0,
                  read_size=100):
-        Input.__init__(self)
-
         self.rate = rate
         self.num_channels = num_channels
         self.amplitude = amplitude
@@ -77,6 +101,7 @@ class EmulatedDaq(Input):
         self._t_per_read = float(self.read_size / self.rate)
 
     def start(self):
+        """Starts the device recording. Not necessary for this device."""
         pass
 
     def read(self):
@@ -104,13 +129,16 @@ class EmulatedDaq(Input):
                 # if we're not meeting real-time requirement, don't wait
                 pass
 
-        data = self._sigma * np.random.randn(self.num_channels, self.read_size)
+        data = self._sigma * numpy.random.randn(self.num_channels,
+                                                self.read_size)
 
         self._t_last_read = time.time()
         return data
 
     def stop(self):
+        """Stops the device recording. Not necessary for this device."""
         pass
 
     def reset(self):
+        """Resets the device parameters. Not necessary for this device."""
         pass
