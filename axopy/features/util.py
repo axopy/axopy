c106e7bbd7b628e1745e89f10101598a7b6e7500
Kenneth Lyons <ixjlyons@gmail.com>
2018-02-12 08:12:07 -0800

    Only exclude 5.10 on travis.

---
+++ axopy/features/util.py
@@ -0,0 +1,161 @@
+"""Utility functions."""
+
+import numpy as np
+
+
+def ensure_2d(array):
+    """Make sure an array has at least 2 dimensions.
+
+    Useful for handling arrays with ``shape=(n,)`` when you want ``(1, n)``.
+
+    Parameters
+    ----------
+    array : array, shape (n,)
+        The input array. If 2-dimensional, nothing is done.
+
+    Returns
+    -------
+    array : array, shape (1, n)
+        The input array as a "row vector".
+    """
+    if array.ndim == 1:
+        array = np.atleast_2d(array)
+    return array
+
+
+def rolling_window(array, n):
+    """Create a rolling window from an array.
+
+    An extra axis is added to efficiently compute statistics over. Use
+    ``axis=-1`` to remove the extra axis.
+
+    Parameters
+    ----------
+    array : ndarray
+        The input array.
+    n : int
+        Window length.
+
+    Returns
+    -------
+    window : array
+        The length-n windows of the input array.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from exgfeatures.util import rolling_window
+    >>> x = np.array([1, 2, 3, 4, 5])
+    >>> rolling_window(x, 2)
+    array([[1, 2],
+           [2, 3],
+           [3, 4],
+           [4, 5]])
+    >>> rolling_window(x, 3)
+    array([[1, 2, 3],
+           [2, 3, 4],
+           [3, 4, 5]])
+    >>> x = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+    >>> rolling_window(x, 2)
+    array([[[1, 2],
+            [2, 3],
+            [3, 4]],
+    <BLANKLINE>
+           [[5, 6],
+            [6, 7],
+            [7, 8]]])
+
+    References
+    ----------
+    .. [1] https://mail.scipy.org/pipermail/numpy-discussion/2010-December/054392.html # noqa
+    """
+    shape = array.shape[:-1] + (array.shape[-1] - n + 1, n)
+    strides = array.strides + (array.strides[-1],)
+    return np.lib.stride_tricks.as_strided(array,
+                                           shape=shape,
+                                           strides=strides)
+
+
+def inverted_t_window(n, p=0.25, a=0.5):
+    """Generate a rectangular window with de-emphasized onset and offset.
+
+    The middle portion of the window is unity, while the onset and offset are
+    flat with less weight, making an inverted "T" shape.
+
+    .. math::
+       w_i =
+       \\begin{cases}
+           1,   & pn \\leq i \\leq (1-p)n \\\\
+           a, & \\text{otherwise}
+       \\end{cases}
+
+    Parameters
+    ----------
+    n : int
+        Number of window samples to generate.
+    p : float, optional
+        Proportion of the window to consider as "onset" and "offset". Should be
+        less than 0.5.
+    a : float, optional
+        Window value during the onset and offset portions. Should be less than
+        one.
+
+    Returns
+    -------
+    w : array, shape (n,)
+        Array of window samples.
+
+    Examples
+    --------
+    >>> import exgfeatures as exg
+    >>> exg.util.inverted_t_window(8)
+    array([ 0.5,  1. ,  1. ,  1. ,  1. ,  1. ,  0.5,  0.5])
+    """
+    w = np.ones(n)
+    w[:int(np.ceil(p * n)) - 1] = a
+    w[int(np.floor((1-p) * n)):] = a
+
+    return w
+
+
+def trapezoidal_window(n, p=0.25):
+    """Generate a symmetric trapezoidal window.
+
+    The middle portion of the window is unity, while the onset and offset are
+    linearly ramped up to 1 and down to 0, respectively.
+
+    .. math::
+       w_i =
+       \\begin{cases}
+           1, & pn \\leq i \\leq (1-p)n \\\\
+           \\frac{i}{pn}, & i < pn \\\\
+           \\frac{i - n}{pn}, & i > (1-p)n
+       \\end{cases}
+
+    Parameters
+    ----------
+    n : int
+        Number of window samples to generate.
+    p : float, optional
+        Proportion of the window to consider as onset and offset. Should be
+        less than 0.5.
+
+    Returns
+    -------
+    w : array, shape (n,)
+        Array of window samples.
+
+    Examples
+    --------
+    >>> import exgfeatures as exg
+    >>> exg.util.trapezoidal_window(9, p=1/3.)
+    array([ 0.33333333,  0.66666667,  1.        ,  1.        ,  1.        ,
+            1.        ,  0.66666667,  0.33333333,  0.        ])
+    """
+    w = np.ones(n)
+    r1 = np.arange(0, int(np.ceil(p * n)) - 1)
+    r2 = np.arange(int(np.floor((1-p) * n)), n)
+    w[r1] = (1/p) * (r1 + 1) / n
+    w[r2] = (1/p) * (n - (r2 + 1)) / n
+
+    return w
---
+++ tests/test_features.py
@@ -0,0 +1,147 @@
+import pytest
+import numpy as np
+from numpy.testing import assert_equal
+import axopy.features as features
+
+
+@pytest.fixture
+def array_2d():
+    return np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+
+
+@pytest.fixture
+def array_1d():
+    return np.array([1, 2, 3, 4, 5])
+
+
+def test_ensure_2d(array_1d, array_2d):
+    assert_equal(features.util.ensure_2d(array_2d), array_2d)
+    assert features.util.ensure_2d(array_1d).ndim == 2
+
+
+def test_rolling_window_1d(array_1d):
+    out = np.array([[1, 2], [2, 3], [3, 4], [4, 5]])
+    assert_equal(features.util.rolling_window(array_1d, 2), out)
+
+
+def test_rolling_window_2d(array_2d):
+    out = np.array([[[1, 2], [2, 3], [3, 4]], [[5, 6], [6, 7], [7, 8]]])
+    assert_equal(features.util.rolling_window(array_2d, 2), out)
+
+
+def test_inverted_t_window():
+    # default params (n = 8)
+    truth = np.array([0.5, 1, 1, 1, 1, 1, 0.5, 0.5])
+    w = features.util.inverted_t_window(8)
+    assert_equal(w, truth)
+
+    # different amplitude (n = 9)
+    truth = np.array([0.3, 0.3, 1, 1, 1, 1, 0.3, 0.3, 0.3])
+    w = features.util.inverted_t_window(9, a=0.3)
+    assert_equal(w, truth)
+
+    # different notch time (n = 100)
+    truth = np.hstack([9*[0.5], np.ones(100-19), 10*[0.5]])
+    w = features.util.inverted_t_window(100, p=0.1)
+    assert_equal(w, truth)
+
+
+def test_trapezoidal_window():
+    # default params
+    truth = np.array([0.5, 1, 1, 1, 1, 1, 0.5, 0])
+    w = features.util.trapezoidal_window(8)
+    assert_equal(w, truth)
+
+    # non-default ramp time
+    truth = np.array([1/3., 2/3., 1, 1, 1, 1, 2/3., 1/3., 0])
+    w = features.util.trapezoidal_window(9, p=1/3.)
+    assert_equal(w, truth)
+
+
+def test_mav():
+    x = np.array([[0, 2], [0, -4]])
+    truth = np.array([1, 2])
+
+    assert_equal(features.mean_absolute_value(x), truth)
+
+
+def test_mav1():
+    x = np.vstack([np.ones(8), np.zeros(8)])
+    # weights should be [0.5, 1, 1, 1, 1, 1, 0.5, 0.5]
+    truth = np.array([0.8125, 0])
+    assert_equal(features.mean_absolute_value(x, weights='mav1'), truth)
+
+
+def test_mav2():
+    x = np.vstack([np.ones(8), np.zeros(8)])
+    # weights should be [0.5, 1, 1, 1, 1, 1, 0.5, 0]
+    truth = np.array([0.75, 0])
+    assert_equal(features.mean_absolute_value(x, weights='mav2'), truth)
+
+
+def test_mav_custom_weights():
+    x = np.ones((4, 10))
+    w = np.zeros(x.shape[1])
+    w[0:2] = 0.4
+    truth = (2*0.4/x.shape[1])*np.ones(x.shape[0])
+
+    assert_equal(features.mean_absolute_value(x, weights=w), truth)
+
+
+def test_mav_bad_weights():
+    # weights not one of the built-in types of MAV
+    with pytest.raises(ValueError):
+        features.mean_absolute_value(np.zeros(2), weights='asdf')
+
+
+def test_mav_bad_custom_weights():
+    # custom weights not the same length as the input data
+    x = np.zeros((4, 10))
+    w = np.zeros(5)
+    with pytest.raises(ValueError):
+        features.mean_absolute_value(x, weights=w)
+
+
+def test_wl():
+    x = np.array([[0, 1, 1, -1], [-1, 2.4, 0, 1]])
+    truth = np.array([3, 6.8])
+
+    assert_equal(features.waveform_length(x), truth)
+
+
+def test_zc():
+    x = np.array([[1, -1, -0.5, 0.2], [1, -1, 1, -1]])
+
+    # zero threshold
+    truth_nothresh = np.array([2, 3])
+    assert_equal(features.zero_crossings(x), truth_nothresh)
+
+    # threshold of 1
+    truth_thresh = np.array([1, 3])
+    assert_equal(features.zero_crossings(x, threshold=1), truth_thresh)
+
+
+def test_ssc():
+    x = np.array([[1, 2, 1.1, 2, 1.2], [1, -1, -0.5, -1.2, 2]])
+
+    # zero threshold
+    truth_nothresh = np.array([3, 3])
+    assert_equal(features.slope_sign_changes(x), truth_nothresh)
+
+    # threshold of one
+    truth_thresh = np.array([0, 2])
+    assert_equal(features.slope_sign_changes(x, threshold=1), truth_thresh)
+
+
+def test_rms():
+    x = np.array([[1, -1, 1, -1], [2, 4, 0, 0]])
+    truth = np.array([1., np.sqrt(5)])
+
+    assert_equal(features.root_mean_square(x), truth)
+
+
+def test_integrated_emg():
+    x = np.array([[-1., 1., -1.], [0, 0, 0]])
+    truth = np.array([3.0, 0])
+
+    assert_equal(features.integrated_emg(x), truth)
