1a11a53430b563edcafdd1c13991c82ebf4fab7d
Kenneth Lyons <ixjlyons@gmail.com>
2018-02-28 07:49:18 -0800

    Added keyboard input device. (#33)

--- examples/minimal.py
+++
@@ -1,7 +0,0 @@
-from axopy.task import Oscilloscope
-from axopy.experiment import Experiment
-from axopy.stream import EmulatedDaq
-
-if __name__ == '__main__':
-    dev = EmulatedDaq(rate=2000, num_channels=4, read_size=200)
-    Experiment([Oscilloscope()], device=dev).run()
