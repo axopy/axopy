31c2f315738c0d4fb88823b0f7147f1f1476cc6c
Kenneth Lyons <ixjlyons@gmail.com>
2018-05-01 14:53:36 -0700

    Fixed gui tests.

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -2,7 +2,7 @@ import pytest
 import os
 from PyQt5 import QtCore, QtWidgets
 from axopy import util
-from axopy.gui.main import MainWindow, Container, SessionInfo
+from axopy.gui.main import MainWindow, Container, SessionConfig
 
 
 def test_main_window(qtbot):
@@ -32,28 +32,13 @@ def test_session_info_widget(qtbot, mock):
                       return_value=QtWidgets.QMessageBox.Ok)
 
     # single configuration session
-    w = SessionInfo()
+    w = SessionConfig({'subject': str, 'group': ('a', 'b')})
 
     # if no subject ID is entered, make sure warning is shown and finished
     # signal isn't emitted
     with qtbot.assertNotEmitted(w.finished):
-        qtbot.mouseClick(w._button, QtCore.Qt.LeftButton)
+        qtbot.keyPress(w, QtCore.Qt.Key_Return)
 
-    qtbot.keyClicks(w._subject_line_edit, 'p0')
-
-    def on_finish_simple(info):
-        assert info == {'subject': 'p0'}
-
-    w.finished.connect(on_finish_simple)
-    qtbot.mouseClick(w._button, QtCore.Qt.LeftButton)
-
-    # multi-configuration session
-    w = SessionInfo(configurations=('a', 'b', 'c'))
-    assert w._config_combo_box.count() == 3
-    qtbot.keyClicks(w._subject_line_edit, 'p1')
-
-    def on_finish(info):
-        assert info == {'subject': 'p1', 'configuration': 'a'}
-
-    w.finished.connect(on_finish)
-    qtbot.mouseClick(w._button, QtCore.Qt.LeftButton)
+    qtbot.keyClicks(w.widgets['subject'], 'p0')
+    qtbot.keyPress(w, QtCore.Qt.Key_Return)
+    assert w.results == {'subject': 'p0', 'group': 'a'}
